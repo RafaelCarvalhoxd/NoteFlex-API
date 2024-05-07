82d54f80e5bb0489a8719ce37f2cd126a98de71f
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-05-07 20:37:34 -0300

    feat: add task gateway

--- src/domain/gateway/task.gateway.ts
+++ src/domain/gateway/task.gateway.ts
@@ -1,6 +1,6 @@
 import { TaskProps } from "../entity/task.entity";
 
-export interface taskGateway {
+export interface TaskGateway {
     save(task: TaskProps): Promise<void>
     list(): Promise<TaskProps[]>
     find(id: string): Promise<TaskProps | null>
