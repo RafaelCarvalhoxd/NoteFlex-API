b5aaf004ceb0eaa8851cd60f7850a1229963570b
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-05-07 18:54:24 -0300

    feat: add domain layer & tag entity

---
+++ src/domain/entity/tag.entity.ts
@@ -0,0 +1,31 @@
+export type TagProps = {
+    id: string;
+    name: string;
+}
+
+export class Tag {
+    private constructor (private prop: TagProps) {}
+
+    public static create (name: string) {
+        return new Tag({
+            id: crypto.randomUUID().toString(),
+            name,
+        });
+    }
+
+    static with(props: TagProps) {
+        return new Tag(props);
+    }
+
+    public get id () {
+        return this.prop.id;
+    }
+
+    public get name () {
+        return this.prop.name;
+    }
+
+    updateName (name: string) {
+        this.prop.name = name;
+    }
+}
