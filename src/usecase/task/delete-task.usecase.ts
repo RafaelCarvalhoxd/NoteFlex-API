4533f43a0a6040e75b768809fa26200847bfcda3
Faelzxd <devrafaelcarvalho14@gmail.com>
2024-05-09 11:58:07 -0300

    feat: add delete task usecase

---
+++ src/usecase/task/delete-task.usecase.ts
@@ -0,0 +1,30 @@
+import { TaskGateway } from "../../domain/gateway/task.gateway";
+import { UseCase } from "../usecase";
+
+export type DeleteTaskInputDto = {
+    id: string;
+}
+
+export type DeleteTaskOutputDto = void
+
+export class DeleteTaskUseCase implements UseCase<DeleteTaskInputDto, DeleteTaskOutputDto>{
+
+    private constructor(private readonly taskGateway: TaskGateway) {}
+
+    public static create(taskGateway: TaskGateway): DeleteTaskUseCase {
+        return new DeleteTaskUseCase(taskGateway);
+    }
+
+    public async execute({id}: DeleteTaskInputDto): Promise<DeleteTaskOutputDto> {
+        const aTask = await this.taskGateway.find(id);
+
+        if (!aTask) {
+            throw new Error('Task not found');
+        }
+
+        await this.taskGateway.delete(id);
+
+        return;
+    }
+
+}
